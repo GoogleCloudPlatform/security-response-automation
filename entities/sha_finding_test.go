a0c89f7c27259ccd5605453e327a5eab59cc4675
Daniel da Silva Andrade <dandrade@ciandt.com>
2019-09-27 11:14:33 -0300

    Additional tests for SHA finding

--- entities/sha_finding_test.go
+++ entities/sha_finding_test.go
@@ -82,3 +82,161 @@ func TestForShaFailures(t *testing.T) {
 		})
 	}
 }
+
+func TestShaSuccess(t *testing.T) {
+	test := []struct {
+		name    string
+		message *pubsub.Message
+	}{
+		{
+			"valid finding",
+			&pubsub.Message{Data: []byte(`{
+				"finding": {
+					"resourceName": "//compute.googleapis.com/projects/teste-project/global/firewalls/6190685430815455733",
+					"category": "OPEN_FIREWALL",
+					"sourceProperties": {
+						"ScannerName": "FIREWALL_SCANNER",
+						"ProjectId": "teste-project"
+					}}}`)},
+		},
+	}
+	for _, tt := range test {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := NewShaFinding().ReadShaFinding(tt.message); err != nil {
+				t.Errorf("exp: nil got:%q", err)
+			}
+		})
+	}
+}
+
+func TestShaReadProjectID(t *testing.T) {
+	test := []struct {
+		name      string
+		message   *pubsub.Message
+		projectID string
+	}{
+		{
+			"Read ProjectID successfully",
+			&pubsub.Message{Data: []byte(`{
+				"finding": {
+					"resourceName": "//compute.googleapis.com/projects/teste-project/global/firewalls/6190685430815455733",
+					"category": "OPEN_FIREWALL",
+					"sourceProperties": {
+						"ScannerName": "FIREWALL_SCANNER",
+						"ProjectId": "teste-project"
+					}}}`)},
+			"teste-project",
+		},
+	}
+	for _, tt := range test {
+		t.Run(tt.name, func(t *testing.T) {
+			f := NewShaFinding()
+			if err := f.ReadShaFinding(tt.message); err != nil {
+				t.Errorf("failed reading SHA finding: %q", err)
+			}
+			z := f.ProjectID()
+			if z != tt.projectID {
+				t.Errorf("%s failed got:%q want:%q", tt.name, z, tt.projectID)
+			}
+		})
+	}
+}
+
+func TestShaReadResourceName(t *testing.T) {
+	test := []struct {
+		name         string
+		message      *pubsub.Message
+		resourceName string
+	}{
+		{
+			"Read ResourceName successfully",
+			&pubsub.Message{Data: []byte(`{
+				"finding": {
+					"resourceName": "//compute.googleapis.com/projects/teste-project/global/firewalls/6190685430815455733",
+					"category": "OPEN_FIREWALL",
+					"sourceProperties": {
+						"ScannerName": "FIREWALL_SCANNER",
+						"ProjectId": "teste-project"
+					}}}`)},
+			"//compute.googleapis.com/projects/teste-project/global/firewalls/6190685430815455733",
+		},
+	}
+	for _, tt := range test {
+		t.Run(tt.name, func(t *testing.T) {
+			f := NewShaFinding()
+			if err := f.ReadShaFinding(tt.message); err != nil {
+				t.Errorf("failed reading SHA finding: %q", err)
+			}
+			z := f.ResourceName()
+			if z != tt.resourceName {
+				t.Errorf("%s failed got:%q want:%q", tt.name, z, tt.resourceName)
+			}
+		})
+	}
+}
+
+func TestShaReadCategory(t *testing.T) {
+	test := []struct {
+		name     string
+		message  *pubsub.Message
+		category string
+	}{
+		{
+			"Read Category successfully",
+			&pubsub.Message{Data: []byte(`{
+				"finding": {
+					"resourceName": "//compute.googleapis.com/projects/teste-project/global/firewalls/6190685430815455733",
+					"category": "OPEN_FIREWALL",
+					"sourceProperties": {
+						"ScannerName": "FIREWALL_SCANNER",
+						"ProjectId": "teste-project"
+					}}}`)},
+			"OPEN_FIREWALL",
+		},
+	}
+	for _, tt := range test {
+		t.Run(tt.name, func(t *testing.T) {
+			f := NewShaFinding()
+			if err := f.ReadShaFinding(tt.message); err != nil {
+				t.Errorf("failed reading SHA finding: %q", err)
+			}
+			z := f.Category()
+			if z != tt.category {
+				t.Errorf("%s failed got:%q want:%q", tt.name, z, tt.category)
+			}
+		})
+	}
+}
+
+func TestShaReadScannerName(t *testing.T) {
+	test := []struct {
+		name        string
+		message     *pubsub.Message
+		scannerName string
+	}{
+		{
+			"Read Category successfully",
+			&pubsub.Message{Data: []byte(`{
+				"finding": {
+					"resourceName": "//compute.googleapis.com/projects/teste-project/global/firewalls/6190685430815455733",
+					"category": "OPEN_FIREWALL",
+					"sourceProperties": {
+						"ScannerName": "FIREWALL_SCANNER",
+						"ProjectId": "teste-project"
+					}}}`)},
+			"FIREWALL_SCANNER",
+		},
+	}
+	for _, tt := range test {
+		t.Run(tt.name, func(t *testing.T) {
+			f := NewShaFinding()
+			if err := f.ReadShaFinding(tt.message); err != nil {
+				t.Errorf("failed reading SHA finding: %q", err)
+			}
+			z := f.ScannerName()
+			if z != tt.scannerName {
+				t.Errorf("%s failed got:%q want:%q", tt.name, z, tt.scannerName)
+			}
+		})
+	}
+}
