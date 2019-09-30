8a49e8a766edfcaae59d4ba56f557a9d4fb44bc4
Tom Fitzgerald <tomfitzgerald@google.com>
2019-09-30 14:37:30 -0700

    add initial deserialization for sha findings

--- go.mod
+++ go.mod
@@ -12,6 +12,8 @@ require (
 	cloud.google.com/go/securitycenter/apiv1p1alpha1 v0.0.0-00010101000000-000000000000
 	github.com/google/go-cmp v0.3.0
 	github.com/pkg/errors v0.8.1
+	golang.org/x/lint v0.0.0-20190909230951-414d861bb4ac // indirect
+	golang.org/x/tools v0.0.0-20190930201159-7c411dea38b0 // indirect
 	google.golang.org/api v0.10.0
 	google.golang.org/genproto v0.0.0-20190905072037-92dd089d5514
 	google.golang.org/genproto/googleapis/cloud/securitycenter/v1p1alpha1 v0.0.0-00010101000000-000000000000
---
+++ providers/sha/findings.go
@@ -0,0 +1,49 @@
+// Package sha holds methods and structures for SHA findings.
+package sha
+
+import (
+	"encoding/json"
+
+	"github.com/googlecloudplatform/threat-automation/entities"
+
+	"cloud.google.com/go/pubsub"
+)
+
+type baseFinding struct {
+	NotificationConfigName string
+	Finding                struct {
+		Name         string
+		Parent       string
+		ResourceName string
+		State        string
+		Category     string
+		ExternalURI  string
+	}
+	SecurityMarks struct {
+		Name string
+	}
+	EventTime  string
+	CreateTime string
+}
+
+// Finding contains fields for a SHA finding.
+type Finding struct {
+	base *baseFinding
+}
+
+// NewFinding deserializes a basic ETD finding from StackDriver.
+func NewFinding(m *pubsub.Message) (*Finding, error) {
+	f := &Finding{}
+	var bf baseFinding
+	if err := json.Unmarshal(m.Data, &bf); err != nil {
+		return f, entities.ErrUnmarshal
+	}
+	f.base = &bf
+	return f, nil
+}
+
+// validate ensures the fields to be accessed by getter methods are valid.
+func (f *Finding) validate() bool {
+	// TODO: Implement this.
+	return true
+}
