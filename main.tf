53c7ea3bca494216694c733f0d9956d352170fe4
Daniel da Silva Andrade <dandrade@ciandt.com>
2019-10-30 14:32:24 -0300

    Load enable bucket only policy module in main terraform file

--- main.tf
+++ main.tf
@@ -69,3 +69,11 @@ module "remove_public_ip" {
     "670032686187",
   ]
 }
+
+module "enable_bucket_only_policy" {
+  source = "./terraform/automations/enable-bucket-only-policy"
+  setup  = "${module.google-setup}"
+  folder-ids = [
+    "670032686187",
+  ]
+}
