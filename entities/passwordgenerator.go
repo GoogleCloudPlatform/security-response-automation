e21fd886ffc97cade71115ecdffedaa5a557d543
Atos Ferreira Machado <atos@ciandt.com>
2019-11-01 14:21:19 -0300

    Adds entity to generate a password

---
+++ entities/passwordgenerator.go
@@ -0,0 +1,35 @@
+package entities
+
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// PasswordGeneratorClient contains minimum interface to generate a password.
+type PasswordGeneratorClient interface {
+	GeneratePassword() string
+}
+
+// PasswordGenerator entity.
+type PasswordGenerator struct {
+	client PasswordGeneratorClient
+}
+
+// NewPasswordGenerator returns a Password Generator entity.
+func NewPasswordGenerator(pg PasswordGeneratorClient) *PasswordGenerator {
+	return &PasswordGenerator{client: pg}
+}
+
+// GeneratePassword generates a password based in a randomly generated uuid.
+func (pg *PasswordGenerator) GeneratePassword() string {
+	return pg.client.GeneratePassword()
+}
