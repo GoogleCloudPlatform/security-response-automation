01aef3bc73be22be6673ff2271c79b8c319f0b99
Kieras <kieras@gmail.com>
2019-11-01 15:08:49 -0300

    Testing when test fails.

--- cloudfunctions/gce/removepublicip/removepublicip_test.go
+++ cloudfunctions/gce/removepublicip/removepublicip_test.go
@@ -171,7 +171,7 @@ func TestRemovePublicIP(t *testing.T) {
 					&externalNic0,
 				},
 			},
-			expectedDeletedAccessConfigs: nil,
+			expectedDeletedAccessConfigs: []stubs.NetworkAccessConfigStub{},
 			folderIDs:                    []string{"456"},
 			ancestry:                     helpers.CreateAncestors([]string{"folder/123"}),
 		},
