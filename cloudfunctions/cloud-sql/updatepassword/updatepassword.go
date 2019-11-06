e2fd9c58398d449be86dc9d3dff424bb9b5e9d33
Atos Ferreira Machado <atos@ciandt.com>
2019-11-06 08:29:40 -0300

    Updates explanation about host name and user name

--- cloudfunctions/cloud-sql/updatepassword/updatepassword.go
+++ cloudfunctions/cloud-sql/updatepassword/updatepassword.go
@@ -32,10 +32,15 @@ type Required struct {
 }
 
 const (
-	// host is the user root host value.
-	host = "%"
-	// userName is the user root name.
-	userName = "root"
+	// defaultHostName is the host name of the default root user
+	// in a MySQL instance. The host name usually is a specific IP address or a
+	// address range that a user can connect from, but the root user can connect
+	// from any host. The symbol "%" means that the host name is unrestricted.
+	defaultHostName = "%"
+	// defaultUserName is the default user name for a Second Generation
+	// Cloud SQL instance that uses MySQL. For more information about it access:
+	// https://cloud.google.com/sql/docs/mysql/users.
+	defaultUserName = "root"
 )
 
 // ReadFinding will attempt to deserialize all supported findings for this function.
@@ -48,8 +53,8 @@ func ReadFinding(b []byte) (*Required, error) {
 	if finding.GetFinding().GetCategory() == "SQL_NO_ROOT_PASSWORD" {
 		r.InstanceName = sha.Instance(finding.GetFinding().GetResourceName())
 		r.ProjectID = finding.GetFinding().GetSourceProperties().GetProjectID()
-		r.Host = host
-		r.UserName = userName
+		r.Host = defaultHostName
+		r.UserName = defaultUserName
 		r.Password = uuid.New().String()
 	}
 	if r.InstanceName == "" || r.ProjectID == "" {
