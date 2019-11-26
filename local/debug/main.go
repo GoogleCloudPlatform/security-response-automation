44ce4130a05b06f780bbc7af941b4187a5368be2
Weslley Alves Assumpcao <walves@ciandt.com>
2019-11-26 09:58:28 -0300

    Adding CF test

--- local/debug/main.go
+++ local/debug/main.go
@@ -49,18 +49,18 @@ const (
 		"logName": "projects/test-project/logs/threatdetection.googleapis.com` + "%%2F" + `detection"
 	}`
 	sccAnoumalousIAMGrant = `{
-		  "notificationConfigName": "organizations/1055058813388/notificationConfigs/noticonf-active-001-id",
+		  "notificationConfigName": "organizations/0000000000000/notificationConfigs/noticonf-active-001-id",
 		  "finding": {
-			"name": "organizations/1055058813388/sources/6961325004191864574/findings/6a30ce604c11417995b1fa260753f3b5\t",
-			"parent": "organizations/1055058813388/sources/6961325004191864574",
-			"resourceName": "//cloudresourcemanager.googleapis.com/projects/714841595327",
+			"name": "organizations/0000000000000/sources/0000000000000000000/findings/6a30ce604c11417995b1fa260753f3b5",
+			"parent": "organizations/0000000000000/sources/0000000000000000000",
+			"resourceName": "//cloudresourcemanager.googleapis.com/projects/000000000000",
 			"state": "ACTIVE",
 			"category": "Persistence: IAM Anomalous Grant",
-			"externalUri": "https://console.cloud.google.com/home?project=long-carving-258319",
+			"externalUri": "https://console.cloud.google.com/home?project=test-project-15511551515",
 			"sourceProperties": {
 			  "detectionCategory_indicator": "audit_log",
 			  "detectionCategory_subRuleName": "external_member_added_to_policy",
-			  "sourceId_customerOrganizationNumber": "1055058813388",
+			  "sourceId_customerOrganizationNumber": "0000000000000",
 			  "evidence_0_sourceLogId_timestamp": "2019-11-22T18:34:34.953Z",
 			  "detectionCategory_ruleName": "iam_anomalous_grant",
 			  "properties_bindingDeltas_0_action": "ADD",
@@ -69,34 +69,54 @@ const (
 			  "detectionCategory_technique": "persistence",
 			  "properties_bindingDeltas_0_role": "roles/editor",
 			  "evidence_0_sourceLogId_insertId": "-xagwy2da2ys",
-			  "properties_principalEmail": "walves@clsecteam.com",
-			  "properties_project_id": "long-carving-258319",
-			  "sourceId_projectNumber": "714841595327",
+			  "properties_principalEmail": "internalmember@someorg.com",
+			  "properties_project_id": "test-project-15511551515",
+			  "sourceId_projectNumber": "000000000000",
 			  "properties_bindingDeltas_1_action": "ADD",
 			  "eventTime": "2019-11-22T18:34:36.153Z",
-			  "properties_bindingDeltas_1_member": "user:weslleyassumpcao@gmail.com",
+			  "properties_bindingDeltas_1_member": "user:externalmember1@gmail.com",
 			  "properties_bindingDeltas_1_role": "roles/editor",
-			  "affectedResources_0_gcpResourceName": "//cloudresourcemanager.googleapis.com/projects/714841595327",
-			  "properties_externalMembers_1": "user:weslleyassumpcao@gmail.com",
-			  "properties_bindingDeltas_0_member": "user:danielsandrade@gmail.com",
-			  "properties_externalMembers_0": "user:danielsandrade@gmail.com"
+			  "affectedResources_0_gcpResourceName": "//cloudresourcemanager.googleapis.com/projects/000000000000",
+			  "properties_externalMembers_1": "user:externalmember1@gmail.com",
+			  "properties_bindingDeltas_0_member": "user:externalmember2@gmail.com",
+			  "properties_externalMembers_0": "user:externalmember2@gmail.com"
 			},
 			"securityMarks": {},
 			"eventTime": "2019-11-22T18:34:36.153Z",
 			"createTime": "2019-11-22T18:34:36.688Z"
 	  	}
 	}`
+	sdAnoumalousIAMGrant = `{
+		"jsonPayload": {
+			"properties": {
+				"project_id": "test-project-15511551515",
+				"principalEmail": "internalmember@someorg.com",
+				"externalMembers": [
+					"user:externalmember2@gmail.com",
+					"user:externalmember1@gmail.com"
+				]
+			},
+			"detectionCategory": {
+				"ruleName": "iam_anomalous_grant",
+				"subRuleName": "external_member_added_to_policy"
+			}
+		},
+		"logName": "projects/test-project/logs/threatdetection.googleapis.com` + "%%2F" + `detection"
+	}`
 )
 
 func main() {
 	ctx := context.Background()
+	if err := exec.SnapshotDisk(ctx, pubsub.Message{Data: []byte(badIP)}); err != nil {
+		log.Fatal(err)
+	}
+	if err := exec.OpenFirewall(ctx, pubsub.Message{Data: []byte(sshBruteForce)}); err != nil {
+		log.Fatal(err)
+	}
 	if err := exec.IAMRevoke(ctx, pubsub.Message{Data: []byte(sccAnoumalousIAMGrant)}); err != nil {
 		log.Fatal(err)
 	}
-	//if err := exec.SnapshotDisk(ctx, pubsub.Message{Data: []byte(badIP)}); err != nil {
-	//	log.Fatal(err)
-	//}
-	//if err := exec.OpenFirewall(ctx, pubsub.Message{Data: []byte(sshBruteForce)}); err != nil {
-	//	log.Fatal(err)
-	//}
+	if err := exec.IAMRevoke(ctx, pubsub.Message{Data: []byte(sdAnoumalousIAMGrant)}); err != nil {
+		log.Fatal(err)
+	}
 }
