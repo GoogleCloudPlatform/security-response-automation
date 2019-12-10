684ba4d08eb73ab02d8625be3c98a948a1d93142
Tom Fitzgerald <tomfitzgerald@google.com>
2019-12-10 10:35:37 -0800

    add badip to read cscc notifications

--- providers/etd/badip/badip.go
+++ providers/etd/badip/badip.go
@@ -1,6 +1,20 @@
 // Package badip represents the bad IP finding.
 package badip
 
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import (
 	"encoding/json"
 
@@ -33,25 +47,43 @@ func (f *Finding) Name(b []byte) string {
 	if err := json.Unmarshal(b, &finding); err != nil {
 		return ""
 	}
+	if f.useCSCC {
+		return f.badIPCSCC.GetFinding().GetSourceProperties().GetDetectionCategoryRuleName()
+	}
 	return finding.JsonPayload.GetDetectionCategory().GetRuleName()
 }
 
-// New returns a new finding.
+type Finding struct {
+	useCSCC   bool
+	badIP     *pb.BadIP
+	badIPCSCC *pb.BadIPSCC
+}
+
 func New(b []byte) (*Finding, error) {
 	var f Finding
 	if err := json.Unmarshal(b, &f.badIP); err != nil {
 		return nil, err
 	}
+	if f.badIP.GetJsonPayload().GetDetectionCategory().GetRuleName() != "" {
+		return &f, nil
+	}
+	if err := json.Unmarshal(b, &f.badIPCSCC); err != nil {
+		return nil, err
+	}
+	f.useCSCC = true
 	return &f, nil
-}
-
-// Finding represents this finding.
-type Finding struct {
-	badIP *pb.BadIP
 }
 
 // CreateSnapshot returns values for the create snapshot automation.
 func (f *Finding) CreateSnapshot() *createsnapshot.Values {
+	if f.useCSCC {
+		return &createsnapshot.Values{
+			ProjectID: f.badIPCSCC.GetFinding().GetSourceProperties().GetPropertiesProjectId(),
+			RuleName:  f.badIPCSCC.GetFinding().GetSourceProperties().GetDetectionCategoryRuleName(),
+			Instance:  etd.Instance(f.badIPCSCC.GetFinding().GetSourceProperties().GetPropertiesInstanceDetails()),
+			Zone:      etd.Zone(f.badIPCSCC.GetFinding().GetSourceProperties().GetPropertiesInstanceDetails()),
+		}
+	}
 	return &createsnapshot.Values{
 		ProjectID: f.badIP.GetJsonPayload().GetProperties().GetProjectId(),
 		RuleName:  f.badIP.GetJsonPayload().GetDetectionCategory().GetRuleName(),
