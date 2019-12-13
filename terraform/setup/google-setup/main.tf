08eb629c94cc61c6c98feee12f9145fa13157b0c
Tom Fitzgerald <tomfitzgerald@google.com>
2019-12-13 10:49:38 -0800

    remove notification config editor from terraform - this is granted during setup via gcloud

--- terraform/setup/google-setup/main.tf
+++ terraform/setup/google-setup/main.tf
@@ -81,12 +81,6 @@ resource "google_pubsub_subscription" "cscc-notifications-subscription" {
   ack_deadline_seconds = 20
 }
 
-resource "google_organization_iam_member" "cscc-notifications-sa" {
-  org_id = var.organization-id
-  role   = "roles/securitycenter.notificationConfigEditor"
-  member = "serviceAccount:${google_service_account.automation-service-account.email}"
-}
-
 resource "google_project_iam_member" "stackdriver-writer" {
   project = var.automation-project
   role    = "roles/logging.logWriter"
