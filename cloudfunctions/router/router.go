daabd0b8ef7668f18e187d5f7945bcc8729d3ab2
Manassés Loiola de Souza <manasses@ciandt.com>
2019-12-19 08:12:30 -0300

    Add IAM Audit Logs Cloud Functions to Router (#141)
    * refactor iam audit logs to router
    
    * refactor iam audit logs terraform
    
    * add router testing + pubsub topic
    
    * add finding declaration
    
    * enable iam audit log module
    
    * fix minor CR issues

--- cloudfunctions/router/router.go
+++ cloudfunctions/router/router.go
@@ -26,6 +26,7 @@ import (
 	"github.com/googlecloudplatform/security-response-automation/providers/etd/badip"
 	"github.com/googlecloudplatform/security-response-automation/providers/sha/computeinstancescanner"
 	"github.com/googlecloudplatform/security-response-automation/providers/sha/datasetscanner"
+	"github.com/googlecloudplatform/security-response-automation/providers/sha/loggingscanner"
 	"github.com/googlecloudplatform/security-response-automation/providers/sha/sqlscanner"
 	"github.com/googlecloudplatform/security-response-automation/providers/sha/storagescanner"
 	"github.com/googlecloudplatform/security-response-automation/services"
@@ -40,6 +41,7 @@ var findings = []Namer{
 	&sqlscanner.Finding{},
 	&computeinstancescanner.Finding{},
 	&datasetscanner.Finding{},
+	&loggingscanner.Finding{},
 }
 
 // Namer represents findings that export their name.
@@ -71,6 +73,7 @@ var topics = map[string]struct{ Topic string }{
 	"cloud_sql_update_password": {Topic: "threat-findings-update-password"},
 	"remove_public_ip":          {Topic: "threat-findings-remove-public-ip"},
 	"close_public_dataset":      {Topic: "threat-findings-close-public-dataset"},
+	"enable_audit_logs":         {Topic: "threat-findings-enable-audit-logs"},
 }
 
 // Configuration maps findings to automations.
@@ -91,6 +94,7 @@ type Configuration struct {
 				SQLNoRootPassword       []sqlscanner.Automation             `yaml:"sql_no_root_password"`
 				PublicIPAddress         []computeinstancescanner.Automation `yaml:"public_ip_address"`
 				PublicDataset           []datasetscanner.Automation         `yaml:"bigquery_public_dataset"`
+				AuditLoggingDisabled    []loggingscanner.Automation         `yaml:"audit_logging_disabled"`
 			}
 		}
 	}
@@ -301,6 +305,26 @@ func Execute(ctx context.Context, values *Values, services *Services) error {
 			switch automation.Action {
 			case "close_public_dataset":
 				values := publicDataset.ClosePublicDataset()
+				values.DryRun = automation.Properties.DryRun
+				topic := topics[automation.Action].Topic
+				if err := publish(ctx, services, automation.Action, topic, values.ProjectID, automation.Target, automation.Exclude, values); err != nil {
+					services.Logger.Error("failed to publish: %q", err)
+					continue
+				}
+			default:
+				return fmt.Errorf("action %q not found", automation.Action)
+			}
+		}
+	case "audit_logging_disabled":
+		automations := services.Configuration.Spec.Parameters.SHA.AuditLoggingDisabled
+		loggingScanner, err := loggingscanner.New(values.Finding)
+		if err != nil {
+			return err
+		}
+		for _, automation := range automations {
+			switch automation.Action {
+			case "enable_audit_logs":
+				values := loggingScanner.EnableAuditLogs()
 				values.DryRun = automation.Properties.DryRun
 				topic := topics[automation.Action].Topic
 				if err := publish(ctx, services, automation.Action, topic, values.ProjectID, automation.Target, automation.Exclude, values); err != nil {
