e70d8cf1d1ff2d7191b5df5b8180e5b08668abc5
Tom Fitzgerald <tom3fitzgerald@gmail.com>
2020-01-07 14:59:07 -0800

    remove changes to depoy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 set -e
-rm ./deploy/functions.zip &2> /dev/null
+rm ./deploy/functions.zip
 zip -r ./deploy/functions.zip . -x *deploy* -x *.git* -x *.terraform*
 terraform apply --auto-approve --target module.$1 .
 echo "Deploying Cloud Function..."
