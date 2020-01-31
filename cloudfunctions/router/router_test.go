3db5e09fe92e73714653f1227b9da6e63322e1ce
Atos Ferreira Machado <atos@ciandt.com>
2020-01-31 16:04:12 -0300

    Fixes the identation of constants

--- cloudfunctions/router/router_test.go
+++ cloudfunctions/router/router_test.go
@@ -33,18 +33,18 @@ import (
 
 const (
 	validBadIP = `{
-			"jsonPayload": {
-				"properties": {
-					"location": "us-central1",
-					"project_id": "test-project",
-					"instanceDetails": "/zones/zone-name/instances/source-instance-name"
-				},
-				"detectionCategory": {
-					"ruleName": "bad_ip"
-				}
+		"jsonPayload": {
+			"properties": {
+				"location": "us-central1",
+				"project_id": "test-project",
+				"instanceDetails": "/zones/zone-name/instances/source-instance-name"
 			},
-			"logName": "projects/test-project/logs/threatdetection.googleapis.com` + "%%2F" + `detection"
-		}`
+			"detectionCategory": {
+				"ruleName": "bad_ip"
+			}
+		},
+		"logName": "projects/test-project/logs/threatdetection.googleapis.com` + "%%2F" + `detection"
+	}`
 	validBadIPSCC = `{
 		"notificationConfigName": "organizations/0000000000000/notificationConfigs/noticonf-active-001-id",
 		"finding": {
@@ -71,84 +71,84 @@ const (
 		}
 	}`
 	ValidSSHBruteForce = `{
-			"jsonPayload": {
-				"properties": {
-					"project_id": "onboarding-project",
-					"loginAttempts": [{
-						"authResult": "FAIL",
-						"sourceIp": "10.200.0.2",
-						"userName": "okokok",
-						"vmName": "ssh-password-auth-debian-9"
-						}, {
-						"authResult": "SUCCESS",
-						"sourceIp": "10.200.0.3",
-						"userName": "okokok",
-						"vmName": "ssh-password-auth-debian-9"
-						}]
-			  },
-			  "detectionCategory": {
-				"ruleName": "ssh_brute_force"
-			  }
+		"jsonPayload": {
+			"properties": {
+				"project_id": "onboarding-project",
+				"loginAttempts": [{
+					"authResult": "FAIL",
+					"sourceIp": "10.200.0.2",
+					"userName": "okokok",
+					"vmName": "ssh-password-auth-debian-9"
+					}, {
+					"authResult": "SUCCESS",
+					"sourceIp": "10.200.0.3",
+					"userName": "okokok",
+					"vmName": "ssh-password-auth-debian-9"
+					}]
 			},
-			"logName": "projects/test-project/logs/threatdetection.googleapis.com` + "%%2F" + `detection"
-		}`
+			"detectionCategory": {
+			"ruleName": "ssh_brute_force"
+			}
+		},
+		"logName": "projects/test-project/logs/threatdetection.googleapis.com` + "%%2F" + `detection"
+	}`
 	validPublicBucket = `{
-			"notificationConfigName": "organizations/154584661726/notificationConfigs/sampleConfigId",
-			"finding": {
-				"name": "organizations/154584661726/sources/2673592633662526977/findings/782e52631d61da6117a3772137c270d8",
-				"parent": "organizations/154584661726/sources/2673592633662526977",
-				"resourceName": "//storage.googleapis.com/this-is-public-on-purpose",
-				"state": "ACTIVE",
-				"category": "PUBLIC_BUCKET_ACL",
-				"externalUri": "https://console.cloud.google.com/storage/browser/this-is-public-on-purpose",
-				"sourceProperties": {
-					"ReactivationCount": 0.0,
-					"ExceptionInstructions": "Add the security mark \"allow_public_bucket_acl\" to the asset with a value of \"true\" to prevent this finding from being activated again.",
-					"SeverityLevel": "High",
-					"Recommendation": "Go to https://console.cloud.google.com/storage/browser/this-is-public-on-purpose, click on the Permissions tab, and remove \"allUsers\" and \"allAuthenticatedUsers\" from the bucket's members.",
-					"ProjectId": "test-project",
-					"AssetCreationTime": "2019-09-19T20:08:29.102Z",
-					"ScannerName": "STORAGE_SCANNER",
-					"ScanRunId": "2019-09-23T10:20:27.204-07:00",
-					"Explanation": "This bucket is public and can be accessed by anyone on the Internet. \"allUsers\" represents anyone on the Internet, and \"allAuthenticatedUsers\" represents anyone who is authenticated with a Google account; neither is constrained to users within your organization."
-				},
-				"securityMarks": {
-					"name": "organizations/154584661726/sources/2673592633662526977/findings/782e52631d61da6117a3772137c270d8/securityMarks",
-					"marks": {
-						"babab": "3"
-					}
-				},
-				"eventTime": "2019-09-23T17:20:27.204Z",
-				"createTime": "2019-09-23T17:20:27.934Z"
-			}
-		}`
+		"notificationConfigName": "organizations/154584661726/notificationConfigs/sampleConfigId",
+		"finding": {
+			"name": "organizations/154584661726/sources/2673592633662526977/findings/782e52631d61da6117a3772137c270d8",
+			"parent": "organizations/154584661726/sources/2673592633662526977",
+			"resourceName": "//storage.googleapis.com/this-is-public-on-purpose",
+			"state": "ACTIVE",
+			"category": "PUBLIC_BUCKET_ACL",
+			"externalUri": "https://console.cloud.google.com/storage/browser/this-is-public-on-purpose",
+			"sourceProperties": {
+				"ReactivationCount": 0.0,
+				"ExceptionInstructions": "Add the security mark \"allow_public_bucket_acl\" to the asset with a value of \"true\" to prevent this finding from being activated again.",
+				"SeverityLevel": "High",
+				"Recommendation": "Go to https://console.cloud.google.com/storage/browser/this-is-public-on-purpose, click on the Permissions tab, and remove \"allUsers\" and \"allAuthenticatedUsers\" from the bucket's members.",
+				"ProjectId": "test-project",
+				"AssetCreationTime": "2019-09-19T20:08:29.102Z",
+				"ScannerName": "STORAGE_SCANNER",
+				"ScanRunId": "2019-09-23T10:20:27.204-07:00",
+				"Explanation": "This bucket is public and can be accessed by anyone on the Internet. \"allUsers\" represents anyone on the Internet, and \"allAuthenticatedUsers\" represents anyone who is authenticated with a Google account; neither is constrained to users within your organization."
+			},
+			"securityMarks": {
+				"name": "organizations/154584661726/sources/2673592633662526977/findings/782e52631d61da6117a3772137c270d8/securityMarks",
+				"marks": {
+					"babab": "3"
+				}
+			},
+			"eventTime": "2019-09-23T17:20:27.204Z",
+			"createTime": "2019-09-23T17:20:27.934Z"
+		}
+	}`
 	validPublicDataset = `{
-			"notificationConfigName": "organizations/154584661726/notificationConfigs/sampleConfigId",
-			"finding": {
-				"name": "organizations/154584661726/sources/7086426792249889955/findings/8682cf07ec50f921172082270bdd96e7",
-				"parent": "organizations/154584661726/sources/7086426792249889955",
-				"resourceName": "//bigquery.googleapis.com/projects/test-project/datasets/public_dataset123",
-				"state": "ACTIVE",
-				"category": "PUBLIC_DATASET",
-				"externalUri": "https://console.cloud.google.com/bigquery?project=test-project&folder&organizationId=154584661726&p=test-project&d=public_dataset123&page=dataset",
-				"sourceProperties": {
-				  "ReactivationCount": 0,
-				  "ExceptionInstructions": "Add the security mark \"allow_public_dataset\" to the asset with a value of \"true\" to prevent this finding from being activated again.",
-				  "SeverityLevel": "High",
-				  "Recommendation": "Go to https://console.cloud.google.com/bigquery?project=test-project&folder&organizationId=154584661726&p=test-project&d=public_dataset123&page=dataset, click \"SHARE DATASET\", search members for \"allUsers\" and \"allAuthenticatedUsers\",  and remove access for those members.",
-				  "ProjectId": "test-project",
-				  "AssetCreationTime": "2019-10-02T18:28:42.182Z",
-				  "ScannerName": "DATASET_SCANNER",
-				  "ScanRunId": "2019-10-03T11:40:22.538-07:00",
-				  "Explanation": "This dataset is public and can be accessed by anyone on the Internet. \"allUsers\" represents anyone on the Internet, and \"allAuthenticatedUsers\" represents anyone who is authenticated with a Google account; neither is constrained to users within your organization."
-				},
-				"securityMarks": {
-				  "name": "organizations/154584661726/sources/7086426792249889955/findings/8682cf07ec50f921172082270bdd96e7/securityMarks"
-				},
-				"eventTime": "2019-10-03T18:40:22.538Z",
-				"createTime": "2019-10-03T18:40:23.445Z"
-			}
-		}`
+		"notificationConfigName": "organizations/154584661726/notificationConfigs/sampleConfigId",
+		"finding": {
+			"name": "organizations/154584661726/sources/7086426792249889955/findings/8682cf07ec50f921172082270bdd96e7",
+			"parent": "organizations/154584661726/sources/7086426792249889955",
+			"resourceName": "//bigquery.googleapis.com/projects/test-project/datasets/public_dataset123",
+			"state": "ACTIVE",
+			"category": "PUBLIC_DATASET",
+			"externalUri": "https://console.cloud.google.com/bigquery?project=test-project&folder&organizationId=154584661726&p=test-project&d=public_dataset123&page=dataset",
+			"sourceProperties": {
+				"ReactivationCount": 0,
+				"ExceptionInstructions": "Add the security mark \"allow_public_dataset\" to the asset with a value of \"true\" to prevent this finding from being activated again.",
+				"SeverityLevel": "High",
+				"Recommendation": "Go to https://console.cloud.google.com/bigquery?project=test-project&folder&organizationId=154584661726&p=test-project&d=public_dataset123&page=dataset, click \"SHARE DATASET\", search members for \"allUsers\" and \"allAuthenticatedUsers\",  and remove access for those members.",
+				"ProjectId": "test-project",
+				"AssetCreationTime": "2019-10-02T18:28:42.182Z",
+				"ScannerName": "DATASET_SCANNER",
+				"ScanRunId": "2019-10-03T11:40:22.538-07:00",
+				"Explanation": "This dataset is public and can be accessed by anyone on the Internet. \"allUsers\" represents anyone on the Internet, and \"allAuthenticatedUsers\" represents anyone who is authenticated with a Google account; neither is constrained to users within your organization."
+			},
+			"securityMarks": {
+				"name": "organizations/154584661726/sources/7086426792249889955/findings/8682cf07ec50f921172082270bdd96e7/securityMarks"
+			},
+			"eventTime": "2019-10-03T18:40:22.538Z",
+			"createTime": "2019-10-03T18:40:23.445Z"
+		}
+	}`
 	validAuditLogDisabled = `{
 		"finding": {
 			"name": "organizations/154584661726/sources/1986930501971458034/findings/1c35bd4b4f6d7145e441f2965c32f074",
@@ -175,8 +175,8 @@ const (
 			"createTime": "2019-10-22T21:01:39.098Z",
 			"assetId": "organizations/154584661726/assets/11190834741917282179",
 			"assetDisplayName": "test-project"
-		   }
-		}`
+		}
+	}`
 	validNonOrgMembers = `{
 		"finding": {
 			"name": "organizations/1050000000008/sources/1986930501000008034/findings/047db1bc23a4b1fb00cbaa79b468945a",
@@ -201,38 +201,38 @@ const (
 			},
 			"eventTime": "2019-10-18T15:30:22.082Z",
 			"createTime": "2019-10-18T15:31:58.487Z"
-           }
-		}`
+		}
+	}`
 	remediatedWebUIEnabled = `{
-			"notificationConfigName": "organizations/154584661726/notificationConfigs/sampleConfigId",
-			"finding": {
-				"name": "organizations/119612413569/sources/7086426792249889955/findings/18db063343328e25a3997efaa0126274",
-				"parent": "organizations/119612413569/sources/7086426792249889955",
-				"resourceName": "//container.googleapis.com/projects/test-cat-findings-clseclab/zones/us-central1-a/clusters/ex-abuse-cluster-3",
-				"state": "ACTIVE",
-				"category": "WEB_UI_ENABLED",
-				"externalUri": "https://console.cloud.google.com/kubernetes/clusters/details/us-central1-a/ex-abuse-cluster-3?project=test-cat-findings-clseclab",
-				"sourceProperties": {
-					"ReactivationCount": 0,
-					"ExceptionInstructions": "Add the security mark \"allow_web_ui_enabled\" to the asset with a value of \"true\" to prevent this finding from being activated again.",
-					"SeverityLevel": "High",
-					"Recommendation": "Go to https://console.cloud.google.com/kubernetes/clusters/details/us-central1-a/ex-abuse-cluster-3?project=test-cat-findings-clseclab then click \"Edit\", click \"Add-ons\", and disable \"Kubernetes dashboard\". Note that a cluster cannot be modified while it is reconfiguring itself.",
-					"ProjectId": "test-cat-findings-clseclab",
-					"AssetCreationTime": "2018-09-26T23:57:19+00:00",
-					"ScannerName": "CONTAINER_SCANNER",
-					"ScanRunId": "2019-09-30T18:20:20.151-07:00",
-					"Explanation": "The Kubernetes web UI is backed by a highly privileged Kubernetes Service Account, which can be abused if compromised. If you are already using the GCP console, the Kubernetes web UI extends your attack surface unnecessarily. Learn more about how to disable the Kubernetes web UI and other techniques for hardening your Kubernetes clusters at https://cloud.google.com/kubernetes-engine/docs/how-to/hardening-your-cluster#disable_kubernetes_dashboard"
-				},
-				"securityMarks": {
-					"name": "organizations/119612413569/sources/7086426792249889955/findings/18db063343328e25a3997efaa0126274/securityMarks",
-					"marks": {
-						"sraRemediated": "50492dc07ec3d961ee8b91fe4addec203ccf23d309eb7d2994dc15aa7f36a6b2"
-                    }
-				},
-				"eventTime": "2019-10-01T01:20:20.151Z",
-				"createTime": "2019-03-05T22:21:01.836Z"
-			}
-		}`
+		"notificationConfigName": "organizations/154584661726/notificationConfigs/sampleConfigId",
+		"finding": {
+			"name": "organizations/119612413569/sources/7086426792249889955/findings/18db063343328e25a3997efaa0126274",
+			"parent": "organizations/119612413569/sources/7086426792249889955",
+			"resourceName": "//container.googleapis.com/projects/test-cat-findings-clseclab/zones/us-central1-a/clusters/ex-abuse-cluster-3",
+			"state": "ACTIVE",
+			"category": "WEB_UI_ENABLED",
+			"externalUri": "https://console.cloud.google.com/kubernetes/clusters/details/us-central1-a/ex-abuse-cluster-3?project=test-cat-findings-clseclab",
+			"sourceProperties": {
+				"ReactivationCount": 0,
+				"ExceptionInstructions": "Add the security mark \"allow_web_ui_enabled\" to the asset with a value of \"true\" to prevent this finding from being activated again.",
+				"SeverityLevel": "High",
+				"Recommendation": "Go to https://console.cloud.google.com/kubernetes/clusters/details/us-central1-a/ex-abuse-cluster-3?project=test-cat-findings-clseclab then click \"Edit\", click \"Add-ons\", and disable \"Kubernetes dashboard\". Note that a cluster cannot be modified while it is reconfiguring itself.",
+				"ProjectId": "test-cat-findings-clseclab",
+				"AssetCreationTime": "2018-09-26T23:57:19+00:00",
+				"ScannerName": "CONTAINER_SCANNER",
+				"ScanRunId": "2019-09-30T18:20:20.151-07:00",
+				"Explanation": "The Kubernetes web UI is backed by a highly privileged Kubernetes Service Account, which can be abused if compromised. If you are already using the GCP console, the Kubernetes web UI extends your attack surface unnecessarily. Learn more about how to disable the Kubernetes web UI and other techniques for hardening your Kubernetes clusters at https://cloud.google.com/kubernetes-engine/docs/how-to/hardening-your-cluster#disable_kubernetes_dashboard"
+			},
+			"securityMarks": {
+				"name": "organizations/119612413569/sources/7086426792249889955/findings/18db063343328e25a3997efaa0126274/securityMarks",
+				"marks": {
+					"sraRemediated": "50492dc07ec3d961ee8b91fe4addec203ccf23d309eb7d2994dc15aa7f36a6b2"
+				}
+			},
+			"eventTime": "2019-10-01T01:20:20.151Z",
+			"createTime": "2019-03-05T22:21:01.836Z"
+		}
+	}`
 )
 
 func TestRouter(t *testing.T) {
