0ade26e58428578dcc3604f462fb67cd08ac6e72
Atos Ferreira Machado <atos@ciandt.com>
2020-02-05 12:06:38 -0300

    Adds test in basdip_test to validate if finding was already remediated

--- providers/etd/badip/badip_test.go
+++ providers/etd/badip/badip_test.go
@@ -35,7 +35,12 @@ func TestBadIP(t *testing.T) {
 				  "properties_instanceDetails": "/projects/test-project-15511551515/zones/us-central1-a/instances/bad-ip-caller",
 				  "properties_location": "us-central1-a"
 			  },
-			  "securityMarks": {},
+			  "securityMarks": {
+				"name": "organizations/0000000000000/sources/0000000000000000000/findings/6a30ce604c11417995b1fa260753f3b5/securityMarks",
+				"marks": {
+					"sraRemediated": "12dcb68e4b5b4e26cb66799cdbb5ae2d92b830428a50e13d1a282fa29a941caf"
+				}
+			  },
 			  "eventTime": "2019-11-22T18:34:36.153Z",
 			  "createTime": "2019-11-22T18:34:36.688Z"
 			}
@@ -53,21 +58,49 @@ func TestBadIP(t *testing.T) {
 			},
 			"logName": "projects/test-project/logs/threatdetection.googleapis.com` + "%%2F" + `detection"
 		}`
+		remediatedBadIPSCC = `{
+			"notificationConfigName": "organizations/0000000000000/notificationConfigs/noticonf-active-001-id",
+			"finding": {
+				"name": "organizations/0000000000000/sources/0000000000000000000/findings/6a30ce604c11417995b1fa260753f3b5",
+				"parent": "organizations/0000000000000/sources/0000000000000000000",
+				"resourceName": "//cloudresourcemanager.googleapis.com/projects/000000000000",
+				"state": "ACTIVE",
+				"category": "C2: Bad IP",
+				"externalUri": "https://console.cloud.google.com/home?project=test-project-15511551515",
+				"sourceProperties": {
+					"detectionCategory_ruleName": "bad_ip",
+					"properties_project_id": "test-project-15511551515",
+					"properties_instanceDetails": "/projects/test-project-15511551515/zones/us-central1-a/instances/bad-ip-caller",
+					"properties_location": "us-central1-a"
+				},
+				"securityMarks": {
+					"name": "organizations/0000000000000/sources/0000000000000000000/findings/6a30ce604c11417995b1fa260753f3b5/securityMarks",
+					"marks": {
+						"sraRemediated": "923ea281b1a02b9573d291310ded7b0885466d612e7d6a709176f180ffd2aa67"
+					}
+				},
+				"eventTime": "2019-11-22T18:34:36.153Z",
+				"createTime": "2019-11-22T18:34:36.688Z"
+			}
+	  }`
+		errorMessage = "Remediation ignored! Finding already processed and remediated. Security Mark: \"sraRemediated:923ea281b1a02b9573d291310ded7b0885466d612e7d6a709176f180ffd2aa67\""
 	)
 
 	for _, tt := range []struct {
-		name     string
-		ruleName string
-		mapTo    []byte
-		finding  []byte
+		name           string
+		ruleName       string
+		mapTo          []byte
+		finding        []byte
+		expectedErrMsg string
 	}{
-		{name: "bad_ip SD", finding: []byte(badIPStackdriver), ruleName: "bad_ip"},
-		{name: "bad_ip CSCC", finding: []byte(badIPSCC), ruleName: "bad_ip"},
+		{name: "bad_ip SD", finding: []byte(badIPStackdriver), ruleName: "bad_ip", expectedErrMsg: ""},
+		{name: "bad_ip CSCC", finding: []byte(badIPSCC), ruleName: "bad_ip", expectedErrMsg: ""},
+		{name: "bad_ip CSCC remediated", finding: []byte(remediatedBadIPSCC), ruleName: "", expectedErrMsg: errorMessage},
 	} {
 		t.Run(tt.name, func(t *testing.T) {
 			f, err := New(tt.finding)
-			if err != nil {
-				t.Fatalf("%q failed: %q", tt.name, err)
+			if err != nil && tt.expectedErrMsg != "" && err.Error() != tt.expectedErrMsg {
+				t.Fatalf("%s failed: got:%q want:%q", tt.name, err, tt.expectedErrMsg)
 			}
 			if name := f.Name(tt.finding); name != tt.ruleName {
 				t.Errorf("%q got:%q want:%q", tt.name, name, tt.ruleName)
